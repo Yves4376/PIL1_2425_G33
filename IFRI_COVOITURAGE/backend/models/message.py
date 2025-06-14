b4367c336841ad4c8031dfe32a211d32bd109953
OKPEIFA-Yves4 <yvesokpeifa642@gmail.com>
2025-06-14 13:40:53 +0100

    ✨ Ajout des modèles : user, trip, message

---
+++ IFRI_COVOITURAGE/backend/models/message.py
@@ -0,0 +1,8 @@
+class Message:
+    def _init_(self, id, match_id, contenu, expediteur_id, destinataire_id, date_envoi):
+        self.id = id
+        self.match_id = match_id
+        self.contenu = contenu
+        self.expediteur_id = expediteur_id
+        self.destinataire_id = destinataire_id
+        self.date_envoi = date_envoi
---
+++ IFRI_COVOITURAGE/backend/models/user.py
@@ -0,0 +1,9 @@
+class User:
+    def _init_(self, id, nom, prenom, email, telephone, mot_de_passe, role):
+        self.id = id
+        self.nom = nom
+        self.prenom = prenom
+        self.email = email
+        self.telephone = telephone
+        self.mot_de_passe = mot_de_passe
+        self.role = role
